ea57436cfe7c4803111170b08b825835bd7cdd6d
BookmDan <danoh329@gmail.com>
2024-01-04 12:43:41 -0800

    initial commit

---
+++ server/migrations/versions/20dc4a614425_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: 20dc4a614425
+Revises: 
+Create Date: 2024-01-04 12:43:24.489727
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '20dc4a614425'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
