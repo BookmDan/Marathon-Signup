e9c5ff37b46392e0b7a212edcadd8a52694c4b0a
BookmDan <danoh329@gmail.com>
2024-01-07 13:23:04 -0800

    seed and app config

--- server/app.py
+++ server/app.py
@@ -6,12 +6,10 @@ from config import app
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-
-db = SQLAlchemy(app)
-migrate = Migrate(app.db)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///your_database.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
+# db = SQLAlchemy(app)
+# migrate = Migrate(app.db)
 
 api = Api(app)
 
--- server/seed.py
+++ server/seed.py
@@ -1,6 +1,21 @@
 from config import app, db
+from app import db, PomAd, User
 
 if __name__ == "__main__":
   with app.app_context():
-    pass
     # remove pass and write your seed data
+    pom_ads = []
+    user1 = User.query.filter_by(username='user1').first()
+    user2 = User.query.filter_by(username='user2').first()
+
+    # Add some PomAd instances to the list
+    pom_ads.append(PomAd(user_id=user1.id, title="Cute male pomeranian puppy", description="Description 1"))
+    pom_ads.append(PomAd(user_id=user2.id, title="AKC pomeranians", description="Description 2"))
+    pom_ads.append(PomAd(user_id=user1.id, title="female pomeranian", description="Description 3"))
+    pom_ads.append(PomAd(user_id=user2.id, title="merle male pomeranian", description="Description 4"))
+
+    # Insert each PomAd in the list into the database table
+    db.session.add_all(pom_ads)
+
+    # Commit the transaction
+    db.session.commit()
