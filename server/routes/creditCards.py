e1c998e32cfb71acb9288b13bdb585be9e804796
BookmDan <danoh329@gmail.com>
2024-01-14 13:57:08 -0800

    clean up code

--- server/routes/creditCards.py
+++ server/routes/creditCards.py
@@ -15,16 +15,16 @@ class CreditCardsResource(Resource):
   def post(self):
     form_data = request.get_json()
     new_credit_card = CreditCardInfo(
-        credit_card_number=form_data.get('credit_card_number'),
-        name_on_card=form_data.get('name_on_card'),
-        expiration_date=form_data.get('expiration_date'),
-        cvv=form_data.get('cvv'),
-        street_address=form_data.get('street_address'),
-        country=form_data.get('country'),
-        zipcode=form_data.get('zipcode'),
-        city=form_data.get('city'),
-        state=form_data.get('state'),
-        save_my_card=form_data.get('save_my_card')
+      credit_card_number=form_data.get('credit_card_number'),
+      name_on_card=form_data.get('name_on_card'),
+      expiration_date=form_data.get('expiration_date'),
+      cvv=form_data.get('cvv'),
+      street_address=form_data.get('street_address'),
+      country=form_data.get('country'),
+      zipcode=form_data.get('zipcode'),
+      city=form_data.get('city'),
+      state=form_data.get('state'),
+      save_my_card=form_data.get('save_my_card')
     )
 
     db.session.add(new_credit_card)
@@ -61,7 +61,7 @@ class CreditCardsById(Resource):
     return make_response(resp, status_code)
   
   def patch(self, id):
-    credit_card = CreditCard.query.filter_by(id=id).first()
+    credit_card = CreditCardInfo.query.filter_by(id=id).first()
     if credit_card:
       form_data = request.get_json()
       for attr in form_data:
@@ -75,7 +75,7 @@ class CreditCardsById(Resource):
       return make_response({"message": f"Credit Card {id} not found"})
 
   def delete(self, id):
-    credit_card = CreditCard.query.filter_by(id=id).first()
+    credit_card = CreditCardInfo.query.filter_by(id=id).first()
     if credit_card:
       db.session.delete(credit_card)
       db.session.commit()
