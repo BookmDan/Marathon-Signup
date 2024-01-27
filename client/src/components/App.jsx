19fe86b5f1baf52f8158b4ea1631276402c675df
BookmDan <danoh329@gmail.com>
2024-01-27 11:37:18 -0800

    update formik add stuff

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,4 @@
-// import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-// import Home from './Home'; // Import your Home component
-// import Posts from './Posts'; // Import your Posts component
-// import Favorites from './Favorites'; // Import your Favorites component
 import NewUser from './NewUser'; // Import your NewUser component
 
 const Navigation = () => (
@@ -19,7 +15,7 @@ const App = () => (
   <Router>
     <Navigation />
     <Routes>
-      <Route path="/register" component={NewUser} /> 
+      <Route path="/register" element={<NewUser/>} /> 
     </Routes>
   </Router>
 );
