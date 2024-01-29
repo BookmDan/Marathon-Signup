27c93fc2292e42db8d8974a2fcf384c7fcfd4784
BookmDan <danoh329@gmail.com>
2024-01-28 18:39:01 -0800

    add routes and links for header.js and app.js

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,16 @@ import { useState, useEffect, createContext } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Header from "./Header";
+import Navigation from "./Navigation"
+import Authentication from "./Authentication"
+import RaceInfo from './RaceInfo';
+import Results from './Results';
+import Photos from './Photos';
+import Volunteer from './Volunteer';
+import Shop from './Shop';
+import RefundPolicy from './RefundPolicy';
+import Directions from './Directions';
+
 
 export const UserContext = createContext(null);
 
@@ -14,33 +24,64 @@ function App() {
   
   const fetchUser = () => {
     fetch('/authorized')
-      .then(res => {
-        if (res.ok) {
-          res.json().then(data => {
-            setUser(data)
-          })
-        } else {
-          setUser(null)
-        }
-      })
+    .then(res => {
+      if (res.ok) {
+        res.json().then(data => {
+          setUser(data)
+        })
+      } else {
+        setUser(null)
+      }
+    })
   }
+
+  const updateUser = (user) => setUser(user)
+  if (!user) return (
+    <>
+      <Navigation />
+      <Authentication udpateUser = {updateUser}/>
+    </>
+  )
   return (
+    <>
+    <Navigation onUpdateUser={updateUser} />
     <Router>
       <div>
         <UserContext.Provider value={[user, setUser]}>
-          <Header />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/Register">
-              <Home />
-            </Route>
-          </Switch>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/register">
+            <RaceInfo />
+          </Route>
+          <Route exact path="/race-info">
+            <RaceInfo />
+          </Route>
+          <Route exact path="/results">
+            <Results />
+          </Route>
+          <Route exact path="/photos">
+            <Photos />
+          </Route>
+          <Route exact path="/volunteer">
+            <Volunteer />
+          </Route>
+          <Route exact path="/shop">
+            <Shop />
+          </Route>
+          <Route exact path="/refund-policy">
+            <RefundPolicy />
+          </Route>
+          <Route exact path="/directions">
+            <Directions />
+          </Route>
+        </Switch>
         </UserContext.Provider>
       </div>
     </Router>
+    </>
   );
 }
-
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,55 @@
-// import React from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import  { useContext } from 'react';
+import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { UserContext } from "./App";
 
 function Header() {
+  const handleLogoutClick = () => {
+    fetch("/api/logout", { method: "DELETE" }).then((r) => {
+      if (r.ok) {
+        setUser(null);
+      }
+    });
+  }
+
+  const [user, setUser] = useContext(UserContext)
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">Race App</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        Race App
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/race-info">Race Info</Nav.Link>
-          <Nav.Link as={Link} to="/results">Results</Nav.Link>
-          <Nav.Link as={Link} to="/photos">Photos</Nav.Link>
-          <Nav.Link as={Link} to="/volunteer">Volunteer</Nav.Link>
+          <Nav.Link as={Link} to="/race-info">
+            Race Info
+          </Nav.Link>
+          <Nav.Link as={Link} to="/results">
+            Results
+          </Nav.Link>
+          <Nav.Link as={Link} to="/photos">
+            Photos
+          </Nav.Link>
+          <Nav.Link as={Link} to="/volunteer">
+            Volunteer
+          </Nav.Link>
           <NavDropdown title="More" id="basic-nav-dropdown">
-            <NavDropdown.Item as={Link} to="/shop">Shop</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/refund-policy">Refund Policy</NavDropdown.Item>
-            <NavDropdown.Item as={Link} to="/directions">Directions</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/shop">
+              Shop
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/refund-policy">
+              Refund Policy
+            </NavDropdown.Item>
+            <NavDropdown.Item as={Link} to="/directions">
+              Directions
+            </NavDropdown.Item>
           </NavDropdown>
         </Nav>
+        {(user)? <Button onClick={handleLogoutClick}>Logout</Button> : ""}
       </Navbar.Collapse>
     </Navbar>
   );
 }
+
 
 export default Header;
