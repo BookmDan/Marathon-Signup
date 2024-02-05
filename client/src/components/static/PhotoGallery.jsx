8615efc4e2ee81697dc3eab8d8fd78c6dd5ed494
BookmDan <danoh329@gmail.com>
2024-02-04 16:56:13 -0800

    one photo works but unable to get gallery to work

--- client/src/components/static/PhotoGallery.jsx
+++ client/src/components/static/PhotoGallery.jsx
@@ -1,27 +1,12 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
+import{ useState } from 'react';
 
-const PhotoGallery = () => {
-  const [photos, setPhotos] = useState([]);
+const PhotoGallery = ({ photoFiles }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const photosPerPage = 8;
 
-  useEffect(() => {
-    // Fetch photos from a certain folder (src/photos/gallery/ in this case)
-    const importAll = (r) => r.keys().map(r);
-    const photoFiles = importAll('../../photos/gallery/', false, /\.(png|jpg|svg)$/);
-
-    // Map file paths to actual image components
-    const photoComponents = photoFiles.map((photo, index) => (
-      <img key={index} src={photo.default} alt={`photo-${index}`} />
-    ));
-
-    setPhotos(photoComponents);
-  }, []);
-
   const indexOfLastPhoto = currentPage * photosPerPage;
   const indexOfFirstPhoto = indexOfLastPhoto - photosPerPage;
-  const currentPhotos = photos.slice(indexOfFirstPhoto, indexOfLastPhoto);
+  const currentPhotos = photoFiles.slice(indexOfFirstPhoto, indexOfLastPhoto);
 
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
@@ -30,12 +15,12 @@ const PhotoGallery = () => {
       <div className="photo-grid">
         {currentPhotos.map((photo, index) => (
           <div key={index} className="photo-item">
-            {photo}
+            <img src={photo.default} alt={`photo-${index}`} />
           </div>
         ))}
       </div>
       <div className="pagination">
-        {Array.from({ length: Math.ceil(photos.length / photosPerPage) }, (_, index) => (
+        {Array.from({ length: Math.ceil(photoFiles.length / photosPerPage) }, (_, index) => (
           <button key={index} onClick={() => paginate(index + 1)}>
             {index + 1}
           </button>
--- client/src/components/static/Photos.jsx
+++ client/src/components/static/Photos.jsx
@@ -1,9 +1,33 @@
-import PhotoGallery from './PhotoGallery';
+// import { useState, useEffect } from 'react';
+// import PhotoGallery from './PhotoGallery';
 import running from "../../photos/gallery/5k.jpg";
 import drone from "../../photos/gallery/drone.jpg";
 
+// const Photos = () => {
+//   const [photoFiles, setPhotoFiles] = useState([]);
+
+//   useEffect(() => {
+//     const importPhotos = async () => {
+//       const context = context('../../photos/gallery/', false, /\.(png|jpe?g|svg)$/);
+//       const photos = await Promise.all(context.keys().map(context));
+//       setPhotoFiles(photos);
+//     };
+
+//     importPhotos();
+//   }, []);
+
+//   return (
+//     <div>
+//       <h2>Photo Gallery</h2>
+//       <PhotoGallery photoFiles={photoFiles} />
+//     </div>
+//   );
+// };
+
+
+
+// <PhotoGallery photoFiles={photoFiles} /> 
 const Photos = () => {
-  // <PhotoGallery />
   return (
     <div>
       <h2>Photo Gallery</h2>
@@ -12,5 +36,4 @@ const Photos = () => {
     </div>
   );
 };
-
 export default Photos;
