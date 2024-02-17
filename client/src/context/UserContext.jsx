6461cbce1763d5c4df04d5b8e851f7abd61917fe
BookmDan <danoh329@gmail.com>
2024-02-17 12:22:00 -0800

    usercontext.jsx created

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,5 +1,21 @@
+import { useState,createContext  } from "react";
+
 const UserContext = createContext({})
 
-const UserProvider = ({ childrn }) => {
-  const [currentUser, setCurrent]
+const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null)
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  const login = (user) => {
+    setCurrentUser(user)
+    setLoggedIn(true)
+  }
+
+  const logout = () => {
+    setCurrentUser(null)
+    setLoggedIn(false)
+  }
+  return <UserContext.Provider value={{currentUser, loggedIn, login, logout}} >{children}</UserContext.Provider>
 }
+
+export {UserContext, UserProvider}
