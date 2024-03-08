0cf3252292c451ea6c2abf9ed2189ee6222d9ac8
BookmDan <danoh329@gmail.com>
2024-03-07 21:08:19 -0800

    working on creating new RaceEvent

--- client/src/components/user-flow/SelectRace.jsx
+++ client/src/components/user-flow/SelectRace.jsx
@@ -10,8 +10,18 @@ const SelectRace = ({ raceEvents, user }) => {
   const navigate = useNavigate();
   const [selectedRaceId, setSelectedRaceId] = useState(null);
   // const [userId, setUserId] = useState(null);
-  const { setSelectedRaceCost } = useCost();
 
+  const { setSelectedRaceCost } = useCost();
+  const [raceName, setRaceName] = useState('');
+  const [organization, setOrganization] = useState('');
+  const [raceType, setRaceType] = useState('');
+  const [raceCost, setRaceCost] = useState('');
+
+  setRaceName('');
+  setOrganization('');
+  setRaceType('');
+  setRaceCost('');
+  
   const handleRaceClick = (selectedRaceId, raceEvent) => {
     setSelectedRaceId(selectedRaceId);
     setSelectedRaceCost(raceEvent.race_cost);
@@ -25,6 +35,31 @@ const SelectRace = ({ raceEvents, user }) => {
       navigate(`/agreement/${selectedRaceId}/${user.id}`);
     }
   };
+
+  const handleAddRaceEvent = () => {
+    const requestBody = {
+      race_name: raceName,
+      organization: organization,
+      race_type: raceType,
+      race_cost: raceCost
+    }
+
+    fetch('/api/race-events', {
+      method: "POST",
+      headers: {
+        "Content-Type": "applications/json",
+      },
+      body:JSON.stringify(requestBody),
+    }).then(res => {
+      if (res.ok) {
+        console.log("Race event added successfully.");
+      } else {
+        throw new Error("Failed to add new race event.");
+      }
+    }).catch(err => {
+      console.error("Error adding new race event:", err)
+    })
+  }
 
   const filteredRaceEvents = raceEvents.filter((event) => {
     const typeMatch = filterType === 'all' || event.race_type === filterType;
@@ -48,6 +83,28 @@ const SelectRace = ({ raceEvents, user }) => {
         <option value="Half Marathon">Half Marathon</option>
         <option value="Full Marathon">Full Marathon</option>
       </select>
+      <div>
+        <h2>Add New Race Event</h2>
+        <form>
+          <div>
+            <label>Race Name:</label>
+            <input type="text" value={raceName} onChange={(e) => setRaceName(e.target.value)} />
+          </div>
+          <div>
+            <label>Organization:</label>
+            <input type="text" value={organization} onChange={(e) => setOrganization(e.target.value)} />
+          </div>
+          <div>
+            <label>Race Type:</label>
+            <input type="text" value={raceType} onChange={(e) => setRaceType(e.target.value)} />
+          </div>
+          <div>
+            <label>Race Cost:</label>
+            <input type="text" value={raceCost} onChange={(e) => setRaceCost(e.target.value)} />
+          </div>
+          <button type="button" onClick={handleAddRaceEvent}>+ Add Race Event</button>
+        </form>
+      </div>
       <div className="race-event-cards">
         {filteredRaceEvents.map((event) => (
           <div
