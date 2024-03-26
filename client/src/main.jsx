4ac05142bc5bfef6c5a5c4fa636b06fa8552f561
BookmDan <danoh329@gmail.com>
2024-03-25 17:01:04 -0700

    going to factory reset

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,17 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './components/App.jsx'
 import { CreditCardProvider } from './context/CreditCardContext.jsx'
 import { UserProvider } from './context/UserContext.jsx'
-// import store from "./redux/store";
-// import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 // value={[user, setUser]}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CreditCardProvider>
-      <UserProvider >
-        <App />
-      </UserProvider>
-    </CreditCardProvider>
+    <Provider store={store}>
+      <CreditCardProvider>
+        <UserProvider >
+          <App />
+        </UserProvider>
+      </CreditCardProvider>
+    </Provider>
   </React.StrictMode>,
 )
-
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,17 @@
-import { createStore } from 'redux';
-import rootReducer from './reducers';
+// import { createStore } from 'redux';
+// import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
-export default store;
+// export default store;
+
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './userSlice';
+import eventReducer from './eventSlice';
+
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+    event: eventReducer,
+  },
+});
